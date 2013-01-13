3f1c15f64e628d3e20abf2ee80ef8b6e591ecc77
gaimande@gmail.com <gaimande@gmail.com@217cf261-d076-e1ef-dccf-6dd4363ec633>
2013-01-13 03:23:48 +0000

    Xong biến trở xoay, ngắt nút bấm. Cần chỉnh sửa code sau khi quay trở về từ LPM phải bắt đầu từ nhãn standby

--- trunk/main.c
+++ trunk/main.c
@@ -14,9 +14,10 @@
 #endif
 
 #define TXD     BIT1		// TXD on P1.1
-#define RXD	BIT2     	// RXD on P1.2
-volatile long tempRaw;
-volatile unsigned int i;
+#define RXD		BIT2     	// RXD on P1.2
+
+volatile unsigned int t1, t2, tempRaw;
+unsigned char flag_timeout, flag_gas;
 
 void FaultRoutine(void);
 void ConfigWDT(void);
@@ -36,94 +37,150 @@ void main(void)
 	ConfigTimerA2();
 	ConfigUART();
 	Print_UART("This is gaimande\n\r");
-	begin:	
+	standby:	
 	_BIS_SR(LPM3_bits + GIE);
-  
-  while(P1IN & BIT5)							// Keep warning til Gas dose not appear
+	
+	if (flag_timeout == 1)
 	{
+		// Buzzer TIME mode
 		P2OUT |= BIT0;							// Buzzer is ON
 		__delay_cycles(10000);					// Delay 80ms, value = (time in second) * (DC0/8)
 		P2OUT &= ~BIT0;							// Buzzer is OFF
 		__delay_cycles(10000);					// Delay 80ms
-	}	
-
-	goto begin;
-	
-  
-  while(1)
-  {
-   P1OUT |= BIT0;
-   for (i = 0; i < 100; i++);
-   P1OUT &= ~BIT0;
-   for (i = 0; i < 5000; i++);
-   }
+		
+	}
+	else if (flag_gas == 1)
+	{
+		P2IE &= ~BIT3; 							// Start INT disable
+		while(P1IN & BIT5)						// Keep warning til Gas dose not appear
+		{
+			// Buzzer GAS_OUT mode
+			P2OUT |= BIT0;							// Buzzer is ON
+			__delay_cycles(10000);					// Delay 80ms, value = (time in second) * (DC0/8)
+			P2OUT &= ~BIT0;							// Buzzer is OFF
+			__delay_cycles(10000);					// Delay 80ms
+		}
+		// Buzzer GAS_OVER mode
+		while(1)						// Keep warning til Gas dose not appear
+		{
+			// Buzzer GAS_OUT mode
+			P2OUT |= BIT0;							// Buzzer is ON
+			__delay_cycles(10000);					// Delay 80ms, value = (time in second) * (DC0/8)
+			P2OUT &= ~BIT0;							// Buzzer is OFF
+			__delay_cycles(700000);					// Delay 80ms
+		}
+		
+	}
+	else
+		goto standby;		
 }
 
 void ConfigWDT(void)
- {
- WDTCTL = WDTPW + WDTHOLD;                 	// Stop watchdog timer
- }
+{
+	WDTCTL = WDTPW + WDTHOLD;                 	// Stop watchdog timer
+}
 
 void ConfigClocks(void)
- {
- if (CALBC1_1MHZ ==0xFF || CALDCO_1MHZ == 0xFF)
-   FaultRoutine();		                    // If calibration data is erased
- 				                            // run FaultRoutine()
-  BCSCTL1 = CALBC1_1MHZ; 					// Set range
-  DCOCTL = CALDCO_1MHZ;  					// Set DCO step + modulation
-  BCSCTL3 |= LFXT1S_2;                      // LFXT1 = VLO
-  IFG1 &= ~OFIFG;                           // Clear OSCFault flag
-  BCSCTL2 |= SELM_0 + DIVM_3 + DIVS_3;      // MCLK = DCO/8, SMCLK = DCO/8
- }
+{
+	if (CALBC1_1MHZ ==0xFF || CALDCO_1MHZ == 0xFF)
+	FaultRoutine();		                    // If calibration data is erased
+											// run FaultRoutine()
+	BCSCTL1 = CALBC1_1MHZ; 					// Set range
+	DCOCTL = CALDCO_1MHZ;  					// Set DCO step + modulation
+	BCSCTL3 |= LFXT1S_2;                    // LFXT1 = VLO
+	IFG1 &= ~OFIFG;                         // Clear OSCFault flag
+	BCSCTL2 |= SELM_0 + DIVM_3 + DIVS_3;    // MCLK = DCO/8, SMCLK = DCO/8
+}
 
 void FaultRoutine(void)
- {
-   P1OUT = BIT0;                            // P1.0 on (red LED)
-   while(1); 			                    // TRAP
- }
+{
+	P1OUT = BIT0;                           // P1.0 on (red LED)
+	while(1); 			                    // TRAP
+}
 
 void ConfigLEDs(void)
- {
-  P1SEL = TXD + RXD;						// P1.1 & 2 TA0, rest GPIO
-  P1SEL2 = TXD + RXD;						// P1.1 & 2 TA0, rest GPIO
+{
+	P1SEL = TXD + RXD;						// P1.1 & 2 TA0, rest GPIO
+	P1SEL2 = TXD + RXD;						// P1.1 & 2 TA0, rest GPIO
 
-  P1DIR = ~(BIT0 + BIT5 + RXD);				// P1.3 input, other outputs
-  P1REN |= BIT0 + BIT5;                     // P1.5 pull-up resistor enable
-  P1IE |= BIT5;                             // Enable P1.5 interrupt
-  P1IES &= ~BIT5;                           // Low to High transition
-  P1IFG &= ~BIT5;                           // Clear interrupt Flag
-  P1OUT = 0;              					// clear output pins
-  P2SEL = 0; 								// P2.6 and 7 GPIO
-  P2DIR |= BIT0 ;	  						// P1.6 and 7 outputs
-  P2OUT = 0;		  				        // clear output pins
- }
+	P1DIR = ~(BIT0 + BIT5 + RXD);			// ADC pin, MQ2 pin adn RXD pin as inputs, other outputs
+	P1REN |= BIT0 + BIT5;                   // Pull-up resistor enable
+
+	P1IE |= BIT5;                           // Enable GAS interrupt
+	P1IES &= ~BIT5;                         // Low to High transition
+	//P1IFG &= ~BIT5;                         // Clear interrupt Flags
+	P1IFG = 0;
+	P2IFG = 0;
+	
+	P2SEL = 0; 								// XIN and XOUT pins as GPIOs
+	P2DIR = ~(BIT3 + BIT4 + BIT5);		  	// Run, Stop and Fao buttons as inputs, other outputs
+	P1REN |= BIT3 + BIT4 + BIT5;            // Pull-up resistor enable
+
+	P2IE |= BIT3 + BIT4 ;          	// Enable Buttons interrupt
+	P2IES |= (BIT3 + BIT4 + BIT5);        	// High to Low transition
+	P2IFG &= ~(BIT3 + BIT4 + BIT5);     	// Clear interrupt Flags
+
+	P1OUT = 0;              				// clear output pins  
+	P2OUT = 0;		  				        // clear output pins
+}
 
 void ConfigADC10(void)
- {
-  ADC10CTL1 = INCH_0 + ADC10DIV_0;        	// INCH_0 selects the analog chanel A0
+{
+	ADC10CTL1 = INCH_0 + ADC10DIV_0;        // INCH_0 selects the analog chanel A0
 											// ADC10DIV_0 selects divide-by-1 as the ADC10 clock 
- }
+}
 
 void ConfigTimerA2(void)
-  {
-   CCTL0 = CCIE;							// Enable capture/compare mode
-   CCR0 = 12000;							// Select the ACLK (VLO) and sets the operation for up mode
-   TACTL = TASSEL_1 + MC_1;					// CCR0 = 12000 x (time in second)
-  }
+{
+	CCTL0 &= ~CCIE;							// Disable capture/compare mode
+	CCR0 = 12000;							// Select the ACLK (VLO) and sets the operation for up mode
+	TACTL = TASSEL_1 + MC_1;				// CCR0 = 12000 x (time in second)
+}
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A (void)
 {
-  ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON;	// SREF_0: selects the range from Vss to Vcc
+	// Read ADC value
+	ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON;	// SREF_0: selects the range from Vss to Vcc
 												// ADC10SHT_3: maximum sample-and-hold time
 												// ADC10ON: turns on the ADC10 peripheral
-  ADC10CTL0 |= ENC + ADC10SC;               // Sampling and conversion start
-  P1OUT |= BIT6; 			                // P1.6 on (green LED)
-  __delay_cycles(100);
-  ADC10CTL0 &= ~ENC;				   		// Disable ADC conversion
-  ADC10CTL0 &= ~ADC10ON;		        	// ADC10 off
-  tempRaw = ADC10MEM;						// Read conversion value
-  P1OUT ^= BIT6; 				                // green LED off
+	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+	P1OUT |= BIT6; 			                // P1.6 on (green LED)
+	//__delay_cycles(100);
+	ADC10CTL0 &= ~ENC;				   		// Disable ADC conversion
+	ADC10CTL0 &= ~ADC10ON;		        	// ADC10 off
+	if (ADC10MEM < 177)
+		tempRaw = 300;
+	else if (ADC10MEM < 326)
+		tempRaw = 600;	
+	else if (ADC10MEM < 484)
+		tempRaw = 900;
+	else if (ADC10MEM < 542)
+		tempRaw = 1200;
+	else if (ADC10MEM < 617)
+		tempRaw = 1500;	
+	else
+		tempRaw = 1800;	
+		
+	if (t1 == tempRaw)		
+	{
+		t2--;
+		if(t2 == 0)
+		{
+			_BIC_SR_IRQ(LPM3_bits);			// LPM off
+			flag_timeout = 1;
+		}
+	}
+	else
+	{
+		// BEEP
+		P2OUT |= BIT0;						// Buzzer is ON
+		__delay_cycles(10000);				// Delay 80ms, value = (time in second) * (DC0/8)
+		P2OUT &= ~BIT0;						// Buzzer is OFF
+		t1 = tempRaw;
+		t2 = t1;
+	}	
+	P1OUT ^= BIT6; 				            // Green LED off
 }
 
 #pragma vector=USCIAB0RX_VECTOR
@@ -135,15 +192,18 @@ __interrupt void USCI0RX_ISR(void)
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT1_ISR(void)
 {    
-	/*
-	while(P1IN & BIT5)							// Keep warning til Gas dose not appear
-	{
-		P2OUT |= BIT0;							// Buzzer is ON
-		__delay_cycles(10000);					// Delay 80ms, value = (time in second) * (DC0/8)
-		P2OUT &= ~BIT0;							// Buzzer is OFF
-		__delay_cycles(10000);					// Delay 80ms
-	}	
-	*/
-	P1IFG &= ~BIT5;         					// Clear interrupt Flag for next warn
-	_BIC_SR_IRQ(LPM3_bits);
+	P1OUT |= BIT3;							// light_gas ON
+	flag_gas = 1;
+	P1IFG &= ~BIT5;         				// Clear interrupt Flag for next warn	
+	_BIC_SR_IRQ(LPM3_bits);					// LPM off
+}
+
+#pragma vector=PORT2_VECTOR
+__interrupt void PORT2_ISR(void)
+{    
+	P1OUT |= BIT4;							// light_gas ON
+	t1 = 0;
+	P2IFG &= ~(BIT3 + BIT4);         		// Clear interrupt Flag for next warn
+	CCTL0 = CCIE;							// Enable capture/compare mode
+	_BIC_SR_IRQ(LPM3_bits);					// LPM off
 }
